4b3548a350b850e73a1a7d4b6826d18dc5ecbb9d
Guy Birchal Robbe - Mac <gbrobbe@gmail.com>
2023-06-10 16:38:25 -0300

    first part of frontend - second try

--- frontend/components/MenuSection/styles.ts
+++ frontend/components/MenuSection/styles.ts
@@ -4,33 +4,34 @@ import styled from 'styled-components';
 export const Container = styled.div<{ active: boolean }>`
   display: flex;
   height: 100vh;
+  background: #rgba(15,54,106);
 `;
 
-export const Menu = styled.button`
-  /* flex: 1; */
-  background: #f1f1f1;
+export const Menu = styled.div`
+  flex: 1;
+  background: rgba(15,54,106);
   display: flex;
   justify-content: flex-end;
   align-items: center;
   width: 30%;
 `;
 
-export const MenuItem = styled.div`
+export const MenuItem = styled.button`
   background: transparent;
   border: none;
   font-size: 16px;
   padding: 8px;
   cursor: pointer;
-  transition: background 0.3s;
+  /* transition: background 0.3s; */
 
   &:hover {
-    background: #d1d1d1;
+    /* background: #d1d1d1; */
   }
 `;
 
 export const ImageContainer = styled.div`
   /* flex: 2.5; */
-  background: #ccc;
+  /* background: #ccc; */
   display: flex;
   justify-content: center;
   align-items: center;
