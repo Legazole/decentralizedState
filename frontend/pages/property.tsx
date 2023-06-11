9dd8bf7a1ef0cbd7ad18005b8cf7e469db9611d1
Guy Birchal Robbe - Mac <gbrobbe@gmail.com>
2023-06-11 03:21:15 -0300

    Property page - buttons

--- frontend/pages/property.tsx
+++ frontend/pages/property.tsx
@@ -3,10 +3,39 @@ import { Container, Image, ImageContainer } from '../styles/pages/property';
 import { Button } from "@taikai/rocket-kit";
 
 export default function Property() {
+  function buyProperty() { }
+  function listProperty() { }
+  function sellProperty() { }
   return (
     <Container>
       <ImageContainer>
-        <Image src="/sea_view.png" alt="" />
+        {/* <Image src="/sea_view.png" alt="" /> */}
+        <Button
+          ariaLabel="Buy"
+          className="button"
+          color="green"
+          value="Connect"
+          variant="solid"
+          action={() => buyProperty()}
+        />
+        <Button
+          ariaLabel="List"
+          className="button"
+          color="green"
+          value="Connect"
+          variant="solid"
+          action={() => listProperty()}
+        />
+        <Button
+          ariaLabel="Sell"
+          className="button"
+          color="green"
+          value="Connect"
+          variant="solid"
+          action={() => sellProperty()}
+
+        />
+
       </ImageContainer>
     </Container>
   );
